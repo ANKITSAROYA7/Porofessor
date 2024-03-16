cd758298066aa8b1b51b15268b6ef768ca7d9f25
ANKITSAROYA7 <ankitsaroya7@gmail.com>
2024-03-16 16:57:06 +0530

    minor_change

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -16,7 +16,7 @@ const HeaderSection = () => {
             <div className='container lg:max-w-[1090px] px-3 mx-auto py-8 sm:pt-10 sm:pb-20 md:pt-14   lg:pt-[99px] lg:pb-[114px] text-center justify-center items-center flex flex-col md:text-start md:items-start md:justify-start'>
                 <p className='text-white ff_Kalavika_bold text-[24px] sm:text-[28px] md:text-[34px] lg:text-[42px] uppercase pb-1 md:pb-2'>Check Stats, Respect The Winrate</p>
                 <p className='text-white text-[19px]  sm:text-[24px] md:text-[29px]  lg:text-4xl  ff_Kalavika_regular font-normal' >Know Your Enemies With Porofessor</p>
-                <p className='text-white  font-semibold text-[16px] sm:text-[18px] lg:text-xl ff_assistant max-w-[505px] pt-5 lg:pt-[46px] '>Use the in-game analytics to crush your opponents:Detailed statistics, build recommendations, and player tags that improve your game!</p>
+                <p className='text-white  font-semibold text-[16px] sm:text-[18px] lg:text-xl ff_assistant max-w-[504px] pt-5 lg:pt-[46px] '>Use the in-game analytics to crush your opponents:Detailed statistics, build recommendations, and player tags that improve your game!</p>
                 <button className='text-white bg-[#6537EB] py-[14px]  px-[41px] flex items-center font-extrabold text-[14px] sm:text-[18px] lg:text-[23px] ff_assistant mt-[35px] lg:mt-[60px] hover:shadow-2xl shadow-[#6537EB]  transition-all duration-300' ><img className=' pe-2' src={Window_logo} alt="#" />DOWNLOAD FOR FREE</button>
                 <div className='flex pt-[13px] flex-col sm:flex-row'>
                     <p className=' ff_assistant font-bold  text-[15px] text-white me-[10px]'> 11.5+ DOWNLOADS</p>
